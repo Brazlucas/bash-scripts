a608611abf38d924177e0783b874d2c0c9577f5b
Brazlucas <107437261+Brazlucas@users.noreply.github.com>
2024-01-27 01:37:35 -0300

    Was added mp3 when terminal open/close :loud_sound:

--- image-j2pa-with-mp3.sh
+++ image-j2pa-with-mp3.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+mp3_file="/home/luqueta/Downloads/duck.mp3"
+mpg123 "$mp3_file" 2>/dev/null &
+
 if ! command -v jp2a &> /dev/null; then
     echo "jp2a is not installed. Please install it before continuing."
     exit 1
 fi
 
 images_directory="/home/luqueta/j2p-custom-imgs/"
+
 
 if [ ! -d "$images_directory" ]; then
     echo "Images directory does not exist: $images_directory"
@@ -23,6 +27,9 @@ image_selected="${images[$(shuf -i 0-$((${#images[@]}-1)) -n 1)]}"
 
 jp2a --color "$image_selected"
 
-sleep 2
+sleep 1
+
+paplay /usr/share/sounds/Yaru/stereo/complete.oga > /dev/null 2>&1 &
 
 clear
+
